9b4a33cf2e59fa4f6742488c8840bdb86b829fd1
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-06-25 19:48:20 -0300

    chore: configurando o lint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,17 +1,17 @@
-import styled, { css } from "styled-components";
+import styled from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-    variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
-    margin: 8px;
-    border-radius: 4px;
-    border: 0;
-    background-color: ${props => props.theme["green-500"]};
-    color: ${props => props.theme.white};
+  width: 100px;
+  height: 40px;
+  margin: 8px;
+  border-radius: 4px;
+  border: 0;
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 `
