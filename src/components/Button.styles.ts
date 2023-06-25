28a191984b317981291f79be5c94553875269ea9
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-06-25 19:25:12 -0300

    feat: configurando estilos globais

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,16 +6,12 @@ interface ButtonContainerProps {
     variant: ButtonVariant;
 }
 
-const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green'
-};
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
-
-    ${props => css`background-color: ${buttonVariants[props.variant]}`}
+    margin: 8px;
+    border-radius: 4px;
+    border: 0;
+    background-color: ${props => props.theme["green-500"]};
+    color: ${props => props.theme.white};
 `
